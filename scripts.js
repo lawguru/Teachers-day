9f504340c9645a23814313cb61a122fa10e8ef28
Dancing bull <72798082+lawguru@users.noreply.github.com>
2024-08-27 23:21:36 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    const revealContent = document.getElementById('revealContent');
+    revealContent.classList.toggle('hidden');
+});
